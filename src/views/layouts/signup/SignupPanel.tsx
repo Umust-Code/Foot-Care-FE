b6fcd1426a1c1f7009e9902f8d52622cadab2b1d
SiHoon61 <dlatlgns000@gmail.com>
2024-10-07 21:20:52 +0900

    fix: 비밀번호 변수 수정

--- src/views/layouts/signup/SignupPanel.tsx
+++ src/views/layouts/signup/SignupPanel.tsx
@@ -17,7 +17,7 @@ const containerCss = css`
 
 type FieldType = {
   id: string;
-  pwd: string;
+  password: string;
   pwdConfirm: string;
   name: string;
   phone: string;
@@ -74,7 +74,7 @@ function SignupPanel() {
         </Form.Item>
 
         <Form.Item<FieldType>
-          name="pwd"
+          name="password"
           label="비밀번호"
           rules={[{ required: true, message: '비밀번호를 입력해주세요.' }]}
         >
@@ -84,7 +84,7 @@ function SignupPanel() {
         <Form.Item<FieldType>
           label="비밀번호 확인"
           name="pwdConfirm"
-          dependencies={['pwd']}
+          dependencies={['password']}
           rules={[
             {
               required: true,
@@ -92,7 +92,7 @@ function SignupPanel() {
             },
             ({ getFieldValue }) => ({
               validator(_, value) {
-                if (!value || getFieldValue('pwd') === value) {
+                if (!value || getFieldValue('password') === value) {
                   return Promise.resolve();
                 }
                 return Promise.reject(new Error('비밀번호가 일치하지 않습니다.'));
